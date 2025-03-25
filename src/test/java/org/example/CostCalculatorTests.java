871d0e990566d048e8fc5675a7045620ebd161ee
khaihd <hoangdangkhai.dev@gmail.com>
2025-03-25 23:18:32 +0700

    add test for hw04

--- src/test/java/org/example/CostCalculatorTests.java
+++ src/test/java/org/example/CostCalculatorTests.java
@@ -6,15 +6,40 @@ import org.junit.jupiter.api.Test;
 
 public class CostCalculatorTests {
 
+//    @Test
+//    void testCalculateCost1() {
+//        int result = CostCalculator.calculateCost(true, 12);
+//        Assertions.assertEquals(360, result);
+//    }
+//
+//    @Test
+//    void testCalculateCost2() {
+//        int result = CostCalculator.calculateCost(false, 1);
+//        Assertions.assertEquals(50, result);
+//    }
+
     @Test
-    void testCalculateCost1() {
+    void testCalculateCostHw04_1() {
+        int result = CostCalculator.calculateCost(false, 1);
+        Assertions.assertEquals(50, result);
+    }
+
+    @Test
+    void testCalculateCostHw04_2() {
+        int result = CostCalculator.calculateCost(false, 12);
+        Assertions.assertEquals(480, result);
+    }
+
+    @Test
+    void testCalculateCostHw04_3() {
+        int result = CostCalculator.calculateCost(true, 1);
+        Assertions.assertEquals(30, result);
+    }
+
+    @Test
+    void testCalculateCostHw04_4() {
         int result = CostCalculator.calculateCost(true, 12);
         Assertions.assertEquals(360, result);
     }
 
-    @Test
-    void testCalculateCost2() {
-        int result = CostCalculator.calculateCost(false, 1);
-        Assertions.assertEquals(50, result);
-    }
 }
